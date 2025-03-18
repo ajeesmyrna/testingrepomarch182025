b845089957b98218d5a2d71e9dded6514913aaeb
ajeesmyrna <ajeesmyrna@outlook.com>
2025-03-18 16:36:32 -0400

    commitfrommain18marchbkp

--- variable.tf
+++ variable.tf
@@ -1,23 +1,3 @@
-variable "subscription_id" {
-  description = "The subscription ID for the Azure account"
-  type        = string
-}
-
-variable "client_id" {
-  description = "The client ID of the service principal"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "The client secret of the service principal"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "The tenant ID of the Azure account"
-  type        = string
-}
-
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
