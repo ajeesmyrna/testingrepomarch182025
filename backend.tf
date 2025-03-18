d9644ecc030393a57c050c06b59118e92527906c
ajeesmyrna <ajeesmyrna@outlook.com>
2025-03-18 14:09:42 -0400

    statefilechange

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     resource_group_name  = "terraformrgmarch2025"
     storage_account_name = "terraformstgmarch2025"
     container_name       = "tfstate"
-    key                  = "terraformmarch18.tfstate"
+    key                  = "terraformmarch182pm.tfstate"
   }
 }
