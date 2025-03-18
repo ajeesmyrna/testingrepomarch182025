0526c0c0229830bfb95fdc482604786decefa156
ajeesmyrna <ajeesmyrna@outlook.com>
2025-03-18 15:08:39 -0400

    commit2

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     resource_group_name  = "terraformrgmarch2025"
     storage_account_name = "terraformstgmarch2025"
     container_name       = "tfstate"
-    key                  = "terraformmarch183pm.tfstate"
+    key                  = "terraformmarch184pm.tfstate"
   }
 }
